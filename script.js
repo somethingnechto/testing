f69d1c5ca7df371c5d194fe13a8ed6f5de52b244
nechto <andrewmrgold@gmail.com>
2024-10-22 18:16:07 +0300

    script

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Колесников Андрей Александрович";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
